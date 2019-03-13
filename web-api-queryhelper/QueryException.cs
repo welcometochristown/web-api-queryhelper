cc7f1511ea4fb12212c3af81e9de3776138784bc
thunderbeans <welcometochristown@gmail.com>
2019-03-13 21:14:00 +0000

    added exception

---
+++ web-api-queryhelper/QueryException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace web_api_queryhelper
+{
+    /// <summary>
+    /// Exception returned if request fails. 
+    ///     Contains [Response] of type HttpReponseMessage
+    /// </summary>
+    public class QueryHelperException : Exception
+    {
+        public HttpResponseMessage Response { get; private set; }
+
+        public QueryHelperException(string message, HttpResponseMessage response)
+            : base(message) { this.Response = response; }
+
+        public QueryHelperException(string message, HttpResponseMessage response, Exception ex)
+          : base(message, ex) { this.Response = response; }
+
+
+    }
+}
